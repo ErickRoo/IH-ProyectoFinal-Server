094b44d4f77b8739988a16354167c16cb31b6901
ErickRoo <erick.rod.ag@outlook.com>
2021-10-19 20:22:57 -0500

    fix commits

--- routes/order.js
+++ routes/order.js
@@ -9,15 +9,6 @@ const orderController = require("../controllers/orderController");
 router.get("/get-all", orderController.getAllOrders);
 
 //POST
-<<<<<<< HEAD
-router.post("/create-rent", orderController.createOrderEquipment);
-
-//PUT
-router.put("/edit-rent", orderController.editOrder);
-
-//DELETE
-router.delete("/delete-rent", orderController.deleteOrder);
-=======
 router.post("/create-order", orderController.createOrderEquipment);
 
 //PUT
@@ -25,6 +16,5 @@ router.put("/edit-order", orderController.editOrder);
 
 //DELETE
 router.delete("/delete-order", orderController.deleteOrder);
->>>>>>> 5e165f7 (add model for rents and sales (together), change the name of the routes)
 
 module.exports = router;
